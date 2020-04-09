332c4bc247af89a18044660c995c0e29bde7d0c9
mageiden <60712721+mageiden@users.noreply.github.com>
2020-04-09 12:43:14 -0700

    change nav bar right

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,32 +2,35 @@ import React from "react";
 
 function Header() {
   return (
-
     <div className="header">
+      <nav class="navbar navbar-expand-lg navbar-light navbar-fixed-top justify-content-between">
 
-    <nav class="navbar navbar-expand-lg navbar-light navbar-fixed-top">
+      <h1>Crown Music</h1>
 
-      <h1> Crown Music </h1>
-
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="md-form active-pink active-pink-2 mb-3 mt-0 searchbar">
-        <input class="form-control" type="text" placeholder="Search" aria-label="Search" />
-      </div>
-
-      <form class="form-inline">
-      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
-          <a class="nav-item nav-link" href="#">Login</a>
-          <a class="nav-item nav-link" href="#">Signup</a>
+      <form class="navbar-nav navbar-form form-inline">
+        <div>
+          <input class="form-control" type="search" placeholder="Search"/>
+          <button class="btn btn-outline-success" type="submit">Search</button>
         </div>
-      </div>
       </form>
 
-    </nav>
 
+      <ul className="navbar-nav ml-auto">
+        <li className="nav-item">
+          <a className="nav-link" href="">Contact</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="">Login</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="">Signup</a>
+        </li>
+      </ul>
+
+    </nav>
     </div>
+
+
   );
 }
 
