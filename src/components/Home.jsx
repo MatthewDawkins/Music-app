72c1c6909f10c88c515666f1f09d15d4747a01fc
Matt <mattd1997@hotmail.com>
2020-04-10 14:56:12 -0400

    add component for adding spotify songs to homepage

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Home(){
+  return (
+    <div className="home">
+      <div>
+      <a src="{{spotify.song.x}}" />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
